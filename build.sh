3b628ca913d2f3dc6b1cf9066b5518e49c7177b8
4iwen <luky.bien@gmail.com>
2024-01-14 22:52:12 +0100

    added back SFML 2.6.x submodule, added CI for building and releasing, build changes

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 rm -rf build
 mkdir build
 cd build
-cmake -DFYZIKS_BUILD_DEMO=ON -DFYZIKS_BUILD_SHARED=ON ..
-cmake --build . --config Release
+cmake -DFYZIKS_BUILD_DEMO=ON -DFYZIKS_BUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=Release ..
+cmake --build .
--- build_visual_studio.bat
+++ build_visual_studio.bat
@@ -1,5 +1,5 @@
 rmdir /s /q build
 mkdir build
 cd build
-cmake -G "Visual Studio 17 2022" -DFYZIKS_BUILD_DEMO=ON -DFYZIKS_BUILD_SHARED=ON ..
-cmake --build . --config Release
+cmake -G "Visual Studio 17 2022" -DFYZIKS_BUILD_DEMO=ON -DFYZIKS_BUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=Release ..
+cmake --build .
